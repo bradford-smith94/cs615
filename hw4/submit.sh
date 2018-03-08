ebfbb22b9714aa3ed8b9d7bc9fc66d3a9f1a9f06
Bradford Smith <bsmith8@stevens.edu>
2018-03-08 17:29:48 -0500

    Add trimmed tcpdump output and commentary
    This adds the trimmed tcpdump output for the first part of the
    assignment and adds some discussion and commentary on it.

--- hw4/submit.sh
+++ hw4/submit.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # Bradford Smith (bsmith8)
 # CS 615 hw4 submit.sh
-# 03/01/2018
+# 03/08/2018
 ##########################
 
 USER=bsmith8
 HW=hw4
 
-tar -cvf ${USER}-${HW}.tar --transform "s/^\./${USER}/" ./*
+tar -cvf ${USER}-${HW}.tar --transform "s/^\\./${USER}/" --exclude \
+    './traceroutes.tcpdump' --exclude '*.tar' ./*
