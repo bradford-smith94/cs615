af16ca9ea6ec403c14affa9b5a742b554b159c45
Bradford Smith <bsmith8@stevens.edu>
2018-03-19 15:43:45 -0400

    Add final learning thoughts and fix submit script
    This adds some final ideas and bits of learning to the assignment
    commentary. It also changes one of the excludes in the submit script so
    that all raw tcpdump outputs are excluded from the submission archive.

--- hw4/submit.sh
+++ hw4/submit.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Bradford Smith (bsmith8)
 # CS 615 hw4 submit.sh
-# 03/08/2018
+# 03/19/2018
 ##########################
 
 USER=bsmith8
 HW=hw4
 
 tar -cvf ${USER}-${HW}.tar --transform "s/^\\./${USER}/" --exclude \
-    './traceroutes.tcpdump' --exclude '*.tar' ./*
+    './*.tcpdump' --exclude '*.tar' ./*
