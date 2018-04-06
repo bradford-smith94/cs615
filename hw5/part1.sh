220ff0117bebfd47179d61ce8c6ea2a943bc9a8e
Bradford Smith <bsmith8@stevens.edu>
2018-04-06 18:18:54 -0400

    Add hw5 files for part 1
    This adds a general hw5 writeup, which already has the setup work I did for the
    assignment. It also adds my part1 script and my test script. The part1.sh script
    assumes the input data has been saved to a file in the current directory called
    'data.gz'. The test.sh script currently only assumes we are testing part1,
    although I'd like to make it work for all parts eventually.

---
+++ hw5/part1.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+ANS1=$(gzcat data.gz | grep ^en | wc -l | tr -d '[:space:]')
+echo "Answer for q1:" $ANS1
+
+ANS2=$(gzcat data.gz | grep ^en | awk '{print $2, $(NF - 1)}' | sort -nrk 2 | head -n 1 | cut -d ' ' -f 1)
+echo "Answer for q2:" $ANS2
+
+ANS3=$(gzcat data.gz | grep ^en | awk '{sum += $NF} END {print sum}')
+echo "Answer for q3:" $ANS3
+
+ANS4=$(gzcat data.gz | grep ^en | awk '{sum += $(NF - 1)} END {print sum / 60 / 60 }')
+echo "Answer for q4:" $ANS4
+
+ANS5=$(gzcat data.gz | grep ^en | awk '{print $NF / $(NF - 1), $0}' | sort -nr | head -n 1 | cut -d ' ' -f 3)
+echo "Answer for q5:" $ANS5
+
+echo "Results:"
+./test.sh "$ANS1" "$ANS2" "$ANS3" "$ANS4" "$ANS5"
---
+++ hw5/test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+HOST=http://ec2-54-145-67-75.compute-1.amazonaws.com/
+PART1=cgi-bin/hw5-1.cgi
+ANSWERS="?q1=$1&q2=$2&q3=$3&q4=$4&q5=$5"
+
+echo $HOST$PART1$ANSWERS
+curl $HOST$PART1$ANSWERS
